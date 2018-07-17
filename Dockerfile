ac811cd7ba7c3dc924d4c972d91563f25470b835
SMX <stefanmehnert@gmail.com>
2018-07-17 13:41:01 +0200

    Add a comment via POST request

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,9 @@
-FROM node:5.11.0-slim
+FROM node:8-alpine
 
 WORKDIR /code
 
-RUN npm install -g nodemon
+RUN npm install -g nodemon@"~1.18.2"
+#RUN npm install -g body-parser@"~1.8.3"
 
 COPY package.json /code/package.json
 RUN npm install && npm ls
--- src/index.js
+++ src/index.js
@@ -1,33 +1,42 @@
 var express = require('express');
 var expressHandlebars  = require('express-handlebars');
+var bodyParser = require('body-parser')
+
 var http = require('http');
 
 var PORT = 8000;
 
 var LINES = [
-    "Hey, now, you're an All Star, get your game on, go play",
-    "Hey, now, you're a Rock Star, get the show on, get paid",
-    "And all that glitters is gold",
-    "Only shooting stars break the mold",
+  "Hey, now, you're an All Star, get your game on, go play",
+  "Hey, now, you're a Rock Star, get the show on, get paid",
+  "And all that glitters is gold",
+  "Only shooting stars break the mold",
 ];
 
 var lineIndex = 0;
 
 var app = express();
+app.use(bodyParser.json());
+
 app.engine('html', expressHandlebars());
 app.set('view engine', 'html');
 app.set('views', __dirname);
 app.get('/', function(req, res) {
-    var message = LINES[lineIndex];
+  var message = LINES[lineIndex];
 
-    lineIndex += 1;
-    if (lineIndex > LINES.length) {
-        lineIndex = 0;
-    }
+  lineIndex += 1;
+  if (lineIndex > LINES.length) {
+      lineIndex = 0;
+  }
 
-    res.render('index', {message: message});
+  res.render('index', {message: message});
+});
+app.post('/entry', function(req, res) {
+  if (!req.body) return res.sendStatus(400)
+  LINES.push(req.body.entry);
+  res.sendStatus(201);
 });
 
 http.Server(app).listen(PORT, function() {
-    console.log("HTTP server listening on port %s", PORT);
+  console.log("HTTP server listening on port %s", PORT);
 });
