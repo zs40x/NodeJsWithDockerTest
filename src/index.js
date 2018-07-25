eb0f33a132dfbb97abbe66e094c7da254c40cea7
SMX <stefanmehnert@gmail.com>
2018-07-25 12:52:52 +0200

    Refactored the bootstrapper code.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 var express = require('express');
 var expressHandlebars  = require('express-handlebars');
 var bodyParser = require('body-parser')
-
 var http = require('http');
 
 var PORT = 8000;
 
-var database = require("./database");
 var app = express();
 app.use(bodyParser.json());
+app.engine('html', expressHandlebars());
+app.set('view engine', 'html');
+app.set('views', __dirname);
 
 var entryModel = require("./entryModel");
 
+var database = require("./database");
 database.getDb(function (err, db) {
   if(!err) {
     db.entries.find({}).each(function (err, doc) {
@@ -22,11 +24,6 @@ database.getDb(function (err, db) {
 
 var controllers = require("./controllers");
 controllers.init(app, database, entryModel);
-
-app.engine('html', expressHandlebars());
-app.set('view engine', 'html');
-app.set('views', __dirname);
-
 
 http.Server(app).listen(PORT, function() {
   console.log("HTTP server listening on port %s", PORT);
