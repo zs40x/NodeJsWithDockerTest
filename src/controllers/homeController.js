d4c4b62ab578096e27f38f077ec3404cf0e84212
SMX <stefanmehnert@gmail.com>
2018-07-25 11:37:33 +0200

    Extracted html page to the homeController.

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -2,6 +2,24 @@
 
     homeController.init = function(app) {
 
+        var LINES = [
+            "Hey, now, you're an All Star, get your game on, go play",
+            "Hey, now, you're a Rock Star, get the show on, get paid",
+            "And all that glitters is gold",
+            "Only shooting stars break the mold",
+          ];
+        var lineIndex = 0;
+
+        app.get('/', function(req, res) {
+            var message = LINES[lineIndex];
+          
+            lineIndex += 1;
+            if (lineIndex > LINES.length) {
+                lineIndex = 0;
+            }
+          
+            res.render('index', {message: message});
+          });
     };
-    
+
 })(module.exports);
--- src/index.js
+++ src/index.js
@@ -6,34 +6,19 @@ var http = require('http');
 
 var PORT = 8000;
 
-var LINES = [
-  "Hey, now, you're an All Star, get your game on, go play",
-  "Hey, now, you're a Rock Star, get the show on, get paid",
-  "And all that glitters is gold",
-  "Only shooting stars break the mold",
-];
-var lineIndex = 0;
-
-var controllers = require("./controllers");
-
 var database = require("./database");
 
 var app = express();
 app.use(bodyParser.json());
 
+
+var controllers = require("./controllers");
+controllers.init(app);
+
 app.engine('html', expressHandlebars());
 app.set('view engine', 'html');
 app.set('views', __dirname);
-app.get('/', function(req, res) {
-  var message = LINES[lineIndex];
 
-  lineIndex += 1;
-  if (lineIndex > LINES.length) {
-      lineIndex = 0;
-  }
-
-  res.render('index', {message: message});
-});
 
 app.get("/entry", function(req,res) {
   database.getDb(function (err, db) {
