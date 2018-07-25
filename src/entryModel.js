800aef6536e78a224f046ff37b5a14ca29e2c6a2
SMX <stefanmehnert@gmail.com>
2018-07-25 13:16:43 +0200

    Removed test data from the entryModel.

--- src/entryModel.js
+++ src/entryModel.js
@@ -1,11 +1,6 @@
 (function (entryModel) {
 
-    var entries = [
-        "Hey, now, you're an All Star, get your game on, go play",
-        "Hey, now, you're a Rock Star, get the show on, get paid",
-        "And all that glitters is gold",
-        "Only shooting stars break the mold",
-      ];
+    var entries = [];
 
     entryModel.all = function() {
         return entries;
